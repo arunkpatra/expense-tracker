b060d3386ecc310eb4d30e2f0b783645e6db6945
arunkpatra <arunkpatra@gmail.com>
2013-12-09 17:15:39 +0530

    Licensing terms added

--- src/main/java/com/exp/tracker/data/entities/AuthEntity.java
+++ src/main/java/com/exp/tracker/data/entities/AuthEntity.java
@@ -1,3 +1,18 @@
+/**
+ * Copyright 2013 Arun K Patra
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+ */
 package com.exp.tracker.data.entities;
 
 import java.io.Serializable;
@@ -13,64 +28,75 @@ import javax.persistence.NamedQueries;
 import javax.persistence.NamedQuery;
 import javax.persistence.Table;
 
-
 @Entity
-@Table(name="authorities")
+@Table(name = "authorities")
 @NamedQueries({
-	@NamedQuery(name="removeAuthorities", query="DELETE AuthEntity a WHERE a.username = :username"),
-	@NamedQuery(name="removeAuthority", query="DELETE AuthEntity a WHERE a.id = :id"),
-	@NamedQuery(name="getAuthorities", query="SELECT a FROM AuthEntity a ORDER BY a.authority")})
-public class AuthEntity implements Serializable{
+        @NamedQuery(name = "removeAuthorities", query = "DELETE AuthEntity a WHERE a.username = :username"),
+        @NamedQuery(name = "removeAuthority", query = "DELETE AuthEntity a WHERE a.id = :id"),
+        @NamedQuery(name = "getAuthorities", query = "SELECT a FROM AuthEntity a ORDER BY a.authority") })
+public class AuthEntity implements Serializable
+{
 
-	private static final long serialVersionUID = 2817266537467589269L;
-	
-	@Id @GeneratedValue(strategy=GenerationType.IDENTITY)
-	private Long id;	
-	private String username;	
-	private String authority;
-	private Long user_id;
-	
-	@ManyToOne (cascade=CascadeType.REMOVE)
-	@JoinColumn(name="user_id", referencedColumnName="id", insertable=false, updatable=false)
-	private UserEntity user;
-	
-	public String getAuthority() {
-		return authority;
-	}
+    private static final long serialVersionUID = 2817266537467589269L;
 
-	public void setAuthority(String authority) {
-		this.authority = authority;
-	}
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String username;
+    private String authority;
+    private Long user_id;
 
-	public Long getId() {
-		return id;
-	}
+    @ManyToOne(cascade = CascadeType.REMOVE)
+    @JoinColumn(name = "user_id", referencedColumnName = "id", insertable = false, updatable = false)
+    private UserEntity user;
 
-	public void setId(Long id) {
-		this.id = id;
-	}
+    public String getAuthority()
+    {
+        return authority;
+    }
 
-	public String getUsername() {
-		return username;
-	}
+    public void setAuthority(String authority)
+    {
+        this.authority = authority;
+    }
 
-	public void setUsername(String username) {
-		this.username = username;
-	}
+    public Long getId()
+    {
+        return id;
+    }
 
-	public UserEntity getUser() {
-		return user;
-	}
+    public void setId(Long id)
+    {
+        this.id = id;
+    }
 
-	public void setUser(UserEntity user) {
-		this.user = user;
-	}
+    public String getUsername()
+    {
+        return username;
+    }
 
-	public Long getUser_id() {
-		return user_id;
-	}
+    public void setUsername(String username)
+    {
+        this.username = username;
+    }
 
-	public void setUser_id(Long userId) {
-		user_id = userId;
-	}
+    public UserEntity getUser()
+    {
+        return user;
+    }
+
+    public void setUser(UserEntity user)
+    {
+        this.user = user;
+    }
+
+    public Long getUser_id()
+    {
+        return user_id;
+    }
+
+    public void setUser_id(Long userId)
+    {
+        user_id = userId;
+    }
 }
