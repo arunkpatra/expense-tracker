5e119b958bcf2d7be82c7653a1d38025a785ca16
Arun Patra <arunkpatra@gmail.com>
2013-12-26 18:11:34 +0530

    Updates

--- src/main/java/com/exp/tracker/app/reports/ReportController.java
+++ src/main/java/com/exp/tracker/app/reports/ReportController.java
@@ -20,13 +20,11 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.multiaction.MultiActionController;
 
@@ -36,103 +34,112 @@ import com.exp.tracker.data.model.UserSettlementBean;
 import com.exp.tracker.services.api.SettlementService;
 
 /**
- * Simple <code>Controller</code> implementation returning
- * data for report rendering. This implementation demonstrates
- * how JasperReports view can be integrated into your application
- * without placing a JasperReports dependency on your application code.
- * All data returned in the <code>ModelAndView</code> instances uses
- * standard Java classes.
- *
+ * Simple <code>Controller</code> implementation returning data for report
+ * rendering. This implementation demonstrates how JasperReports view can be
+ * integrated into your application without placing a JasperReports dependency
+ * on your application code. All data returned in the <code>ModelAndView</code>
+ * instances uses standard Java classes.
+ * 
  * @author Rob Harrop
  */
 @Controller
 @RequestMapping("/reports")
-public class ReportController extends MultiActionController {
+public class ReportController extends MultiActionController
+{
 
     @Autowired
-	private SettlementService settlementService;
-	/**
-	 * Returns model and view for settlement sub report
-	 */
-	@RequestMapping(value = "/settlementSubReport.pdf", method = RequestMethod.GET)
-	public ModelAndView handleSettlementSubReport(HttpServletRequest request,
-			HttpServletResponse response) throws Exception {
-		String settlementId = (String) request.getParameter("sid");
-		ModelAndView mv = new ModelAndView("settlementSubReport", getModelForSettlementSubReport(settlementId));
-		//
-		System.out.println("Generating Settlement report for settlement # " + settlementId);
-		//
-		return mv;
-	}
-	
-	/**
-	 * Returns model and view for expense report.
-	 * 
-	 * @param request
-	 * @param response
-	 * @return
-	 * @throws Exception
-	 */
-	@RequestMapping(value = "/expenseReport.pdf", method = RequestMethod.GET)
-	public ModelAndView handleExpenseReport(HttpServletRequest request,
-			HttpServletResponse response) throws Exception {
-		String settlementId = (String) request.getParameter("sid");
-		ModelAndView mv = new ModelAndView("expenseReport", getModelForExpenseReport(settlementId));
-		//
-		System.out.println("Generating Expense report for settlement # " + settlementId);
-		//
-		return mv;
-	}
-	
-	/**
-	 * Returns model for expense report
-	 * @param settlementId
-	 * @return
-	 */
-	private Map<String, ?> getModelForExpenseReport(String settlementId) {
-		Map<String, Object> model = new HashMap<String, Object>();
-		// get data from back end
-		Long sid = Long.parseLong(settlementId);
-		List<ExpenseReportDataBean> erdbList = settlementService.getExpensesForSettlementId(sid);
-		SettlementBean sb = settlementService.getSettlementById(sid);
-		// put parameters
-		model.put("reportTitle", "Expense Report");		
-		model.put("startDate", sb.getStartDate());
-		model.put("endDate", sb.getEndDate());
-		model.put("totalVolume", sb.getVolume());
-		// put detail record data
-		model.put("dataSource", erdbList);
-		// return
-		return model;
-	}
-	/**
-	 * Returns model data for settlement sub report.
-	 * 
-	 * @param settlementId
-	 * @return
-	 */
-	private Map<String, ?> getModelForSettlementSubReport(String settlementId) {
-		Map<String, Object> model = new HashMap<String, Object>();
-		// get data from back end
-		Long sid = Long.parseLong(settlementId);
-		SettlementBean sb = settlementService.getSettlementById(sid);
-		// set params
-		model.put("reportTitle", "Settlement Report");		
-		model.put("startDate", sb.getStartDate());
-		model.put("endDate", sb.getEndDate());
-		model.put("volume", sb.getVolume());
-		//
-		List<UserSettlementBean> usbList = sb.getUserSettlementList();
-		// get data
-		model.put("dataSource", usbList);
-		return model;
-	}
+    private SettlementService settlementService;
 
-	public SettlementService getSettlementService() {
-		return settlementService;
-	}
+    /**
+     * Returns model and view for settlement sub report.
+     * 
+     * @param sid
+     *            Settlement Id
+     * @return
+     * @throws Exception
+     */
+    @RequestMapping(value = "/settlementSubReport.pdf", method = RequestMethod.GET)
+    public ModelAndView handleSettlementSubReport(
+            @RequestParam("sid") String sid) throws Exception
+    {
+        ModelAndView mv = new ModelAndView("settlementSubReport",
+                getModelForSettlementSubReport(sid));
+        return mv;
+    }
 
-	public void setSettlementService(SettlementService settlementService) {
-		this.settlementService = settlementService;
-	}
+    /**
+     * Returns model and view for expense report.
+     * 
+     * @param sid
+     *            Settlement Id
+     * @return
+     * @throws Exception
+     */
+    @RequestMapping(value = "/expenseReport.pdf", method = RequestMethod.GET)
+    public ModelAndView handleExpenseReport(@RequestParam("sid") String sid)
+            throws Exception
+    {
+        ModelAndView mv = new ModelAndView("expenseReport",
+                getModelForExpenseReport(sid));
+        return mv;
+    }
+
+    /**
+     * Returns model for expense report
+     * 
+     * @param settlementId
+     * @return
+     */
+    private Map<String, ?> getModelForExpenseReport(String settlementId)
+    {
+        Map<String, Object> model = new HashMap<String, Object>();
+        // get data from back end
+        Long sid = Long.parseLong(settlementId);
+        List<ExpenseReportDataBean> erdbList = settlementService
+                .getExpensesForSettlementId(sid);
+        SettlementBean sb = settlementService.getSettlementById(sid);
+        // put parameters
+        model.put("reportTitle", "Expense Report");
+        model.put("startDate", sb.getStartDate());
+        model.put("endDate", sb.getEndDate());
+        model.put("totalVolume", sb.getVolume());
+        // put detail record data
+        model.put("dataSource", erdbList);
+        // return
+        return model;
+    }
+
+    /**
+     * Returns model data for settlement sub report.
+     * 
+     * @param settlementId
+     * @return
+     */
+    private Map<String, ?> getModelForSettlementSubReport(String settlementId)
+    {
+        Map<String, Object> model = new HashMap<String, Object>();
+        // get data from back end
+        Long sid = Long.parseLong(settlementId);
+        SettlementBean sb = settlementService.getSettlementById(sid);
+        // set params
+        model.put("reportTitle", "Settlement Report");
+        model.put("startDate", sb.getStartDate());
+        model.put("endDate", sb.getEndDate());
+        model.put("volume", sb.getVolume());
+        //
+        List<UserSettlementBean> usbList = sb.getUserSettlementList();
+        // get data
+        model.put("dataSource", usbList);
+        return model;
+    }
+
+    public SettlementService getSettlementService()
+    {
+        return settlementService;
+    }
+
+    public void setSettlementService(SettlementService settlementService)
+    {
+        this.settlementService = settlementService;
+    }
 }
