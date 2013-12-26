60f38c6f25f2e1bb30ba9149ff91e7773181055f
Arun Patra <arunkpatra@gmail.com>
2013-12-26 17:56:38 +0530

    Settlement Module fixes

--- src/main/java/com/exp/tracker/app/reports/ReportController.java
+++ src/main/java/com/exp/tracker/app/reports/ReportController.java
@@ -23,6 +23,10 @@ import java.util.Map;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.multiaction.MultiActionController;
 
@@ -41,12 +45,16 @@ import com.exp.tracker.services.api.SettlementService;
  *
  * @author Rob Harrop
  */
+@Controller
+@RequestMapping("/reports")
 public class ReportController extends MultiActionController {
 
+    @Autowired
 	private SettlementService settlementService;
 	/**
 	 * Returns model and view for settlement sub report
 	 */
+	@RequestMapping(value = "/settlementSubReport.pdf", method = RequestMethod.GET)
 	public ModelAndView handleSettlementSubReport(HttpServletRequest request,
 			HttpServletResponse response) throws Exception {
 		String settlementId = (String) request.getParameter("sid");
@@ -65,6 +73,7 @@ public class ReportController extends MultiActionController {
 	 * @return
 	 * @throws Exception
 	 */
+	@RequestMapping(value = "/expenseReport.pdf", method = RequestMethod.GET)
 	public ModelAndView handleExpenseReport(HttpServletRequest request,
 			HttpServletResponse response) throws Exception {
 		String settlementId = (String) request.getParameter("sid");
