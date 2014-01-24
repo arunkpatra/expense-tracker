5bf67b80574676527baacd28766c45b30521323b
Arun Patra <arunkpatra@gmail.com>
2014-01-24 12:44:28 +0530

    Updates

---
+++ src/main/resources/scripts/expense-tracker-drop-objects.sql
@@ -0,0 +1,13 @@
+--
+---- DROP IN THIS SEQUENCE
+--
+drop table et_payments;
+drop table et_audit;
+drop table et_user_expense;
+drop table et_expense;
+drop table et_user_settlement;
+drop table et_reports;
+drop table et_settlement;
+drop table authorities;
+drop table roles;
+drop table users;
