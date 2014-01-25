950415f102a80bd9b409637dc757b76c849b68b6
Arun K Patra <arunkpatra@gmail.com>
2014-01-25 12:59:00 +0530

    Strange test failure

--- src/test/java/com/exp/tracker/services/impl/JpaExpenseServiceTests.java
+++ src/test/java/com/exp/tracker/services/impl/JpaExpenseServiceTests.java
@@ -46,7 +46,7 @@ public class JpaExpenseServiceTests
         ub1.setPassword("password");
         ub1.setUsername("testuser1");      
         UserBean userBean1 = userService.addUser(ub1);
-        Assert.assertNotNull("Failed to create user1", userBean1);
+        Assert.assertNotNull("Failed to create user1. Why?", userBean1);
         //
         // Add 1st user
         UserBean ub2 = new UserBean();
