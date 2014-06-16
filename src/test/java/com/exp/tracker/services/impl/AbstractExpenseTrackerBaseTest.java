006827e5702121fb2ebe8f814cf6a2be74b757f0
Arun Patra <arunkpatra@gmail.com>
2014-06-16 19:34:52 +0530

    Added import

--- src/test/java/com/exp/tracker/services/impl/AbstractExpenseTrackerBaseTest.java
+++ src/test/java/com/exp/tracker/services/impl/AbstractExpenseTrackerBaseTest.java
@@ -5,6 +5,7 @@ import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.transaction.annotation.Transactional;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration("classpath:spring/root-applicationContext.xml")
