ced9cff0de2c68cdadb2cf554d74733c5090e47e
Arun K Patra <arunkpatra@gmail.com>
2014-06-22 03:38:36 +0530

    License

--- src/test/java/com/exp/tracker/services/impl/GroupServiceImplTests.java
+++ src/test/java/com/exp/tracker/services/impl/GroupServiceImplTests.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2013 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.exp.tracker.services.impl;
 
 import java.util.Collection;
@@ -6,6 +22,7 @@ import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.binding.message.Message;
 import org.springframework.context.ApplicationContext;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
@@ -18,23 +35,27 @@ import org.springframework.webflow.test.MockRequestContext;
 
 import com.exp.tracker.data.model.GroupBean;
 import com.exp.tracker.services.api.GroupService;
+import com.exp.tracker.services.api.ServiceOperationValidationService;
 
 public class GroupServiceImplTests extends AbstractExpenseTrackerBaseTest {
 
 	@Autowired
 	private GroupService groupService;
-	
+
+	@Autowired
+	private ServiceOperationValidationService validationService;
+
 	@Autowired
 	ApplicationContext ctx;
 	private RequestContext rCtx;
-	
+
 	static JdbcDaoImpl userDetailService;
-	
+
 	@Before
 	public void setup() {
 
 	}
-	
+
 	@Test
 	public void groupServiceTests() {
 		userDetailService = ctx.getBean(JdbcDaoImpl.class);
@@ -47,14 +68,14 @@ public class GroupServiceImplTests extends AbstractExpenseTrackerBaseTest {
 		MockExternalContext ec = new MockExternalContext();
 		ec.setCurrentUser("Admin");
 		((MockRequestContext) rCtx).setExternalContext(ec);
-		
+
 		// This test runs with two groups already setup in the embedded test db
-		
+
 		// get all groups
 		Collection<GroupBean> groups = groupService.getGroups();
 		Assert.assertNotNull("Expected a non null group list", groups);
 		Assert.assertTrue("Expected exactly two groups", groups.size() == 2);
-		
+
 		// Add a group
 		GroupBean gb = new GroupBean();
 		gb.setActive(true);
@@ -65,6 +86,23 @@ public class GroupServiceImplTests extends AbstractExpenseTrackerBaseTest {
 		Assert.assertNotNull("Failed to create group", retGb);
 		groups = groupService.getGroups();
 		Assert.assertTrue("Expected exactly three groups", groups.size() == 3);
-		
+		//
+		validationService.checkGroupCreationResult(retGb, rCtx);
+		Message[] msgs = rCtx.getMessageContext().getAllMessages();
+		Assert.assertTrue("Expected one message", msgs.length == 1);
+		Message msg = msgs[0];
+		Assert.assertTrue("Expected a success message",
+				"Your group was created succesfuly.".equals(msg.getText()));
+		// clear this message
+		rCtx.getMessageContext().clearMessages();
+		// now try adding group again with same name
+		retGb = groupService.addGroup(gb);
+		Assert.assertNull("Should have failed to create group", retGb);
+		validationService.checkGroupCreationResult(retGb, rCtx);
+		msgs = rCtx.getMessageContext().getAllMessages();
+		Assert.assertTrue("Expected one message", msgs.length == 1);
+		msg = msgs[0];
+		Assert.assertTrue("Expected a failure message",
+				"Failed to create group.".equals(msg.getText()));
 	}
 }
